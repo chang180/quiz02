bcf479f2a7e9f41dc37b80c24e5c50396b20d0e3
張 建文 <chang180@gmail.com>
2020-11-17 08:49:34 +0800

    add some models

---
+++ database/migrations/2020_11_17_002213_create_classifies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassifiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classifies', function (Blueprint $table) {
+            $table->id();
+            $table->string('classify',120)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classifies');
+    }
+}
